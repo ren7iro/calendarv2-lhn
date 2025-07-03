96ac3f47411378b297d57625871dfd08379602fe
PS <41328987+ren7iro@users.noreply.github.com>
2025-07-03 12:22:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const csvURL = 'data/calendar_lhn_flattened.csv';
+const csvURL = 'calendar_lhn_flattened.csv';
 
 function formatTime(date) {
   return `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
